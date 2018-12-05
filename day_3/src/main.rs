4b9d83ccacbcf817cbd731ee56f1ce16864c6a72
Pearce Keesling <keeslinp@gmail.com>
2018-12-05 10:12:11 -0700

    day 3 pt 2

--- day_3/src/main.rs
+++ day_3/src/main.rs
@@ -18,7 +18,9 @@ fn main() {
     let mut f = File::open(filename).expect("File not Found");
     let mut contents = String::new();
     f.read_to_string(&mut contents).expect("Could not read file contents");
-    let mut grid = [[0u32; 2000]; 1000];
+    let mut grid = [[(0u16, 0u16); 2000]; 1000];
+    let mut doubled = [false; 2000];
+    doubled[0] = true;
     let re = Regex::new(r#"#(\d+) @ (\d+),(\d+): (\d+)x(\d+)"#).expect("regex failed");
     re.captures_iter(&contents).map(|cap| {
         Square {
@@ -26,15 +28,23 @@ fn main() {
             position: (cap.get(2).unwrap().as_str().parse::<u16>().unwrap(), cap.get(3).unwrap().as_str().parse::<u16>().unwrap()),
             size: (cap.get(4).unwrap().as_str().parse::<u16>().unwrap(), cap.get(5).unwrap().as_str().parse::<u16>().unwrap()),
         }
-    }).for_each(|Square { position: (xpos, ypos), size: (width,  height), .. }| {
+    }).for_each(|Square { position: (xpos, ypos), size: (width,  height), id }| {
         for x in 0..width {
             for y in 0..height {
-                grid[(ypos + y) as usize][(xpos + x) as usize] += 1;
+                let (old_id, count) = grid[(ypos + y) as usize][(xpos + x) as usize];
+                if old_id != 0 {
+                    doubled[id as usize] = true;
+                    doubled[old_id as usize] = true;
+                }
+                grid[(ypos + y) as usize][(xpos + x) as usize] = (id, count + 1);
             }
         }
     });
-    let count = grid.iter().fold(0, |sum, row| {
-        sum + row.iter().fold(0, |agg, val| if *val > 1 { agg + 1 } else { agg })
+    let part1 = grid.iter().fold(0, |sum, row| {
+        sum + row.iter().fold(0, |agg, val| if val.1 > 1 { agg + 1 } else { agg })
     });
-    println!("{}", count);
+    println!("{}", part1);
+
+    let index = doubled.iter().position(|val| !val).expect("could not find a undoubled value");
+    println!("{}", index);
 }
